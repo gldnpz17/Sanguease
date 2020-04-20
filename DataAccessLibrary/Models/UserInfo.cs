ff65d13de5aba3b9648c0952404639109ac97a1e
firdausbismasuryakusuma <firdausbismasuryakusuma@365.ugm.ac.id>
2020-04-20 23:15:55 +0700

    updated DBDesign
    added UserInfo model properties

--- DataAccessLibrary/Models/UserInfo.cs
+++ DataAccessLibrary/Models/UserInfo.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataAccessLibrary.Models
 {
     class UserInfo
     {
-        
+        public string Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR(128)")]
+        public string FullName { get; set; }
+        public List<InventoryItem> Inventory { get; set; }
+        public List<AchievementItem> Achievements { get; set; }
     }
 }
